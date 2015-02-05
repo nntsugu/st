703fedbe3bb730126c6be725d313294bc2da6ac7
moji <nntsugu@gmail.com>
2015-02-05 20:48:18 +0900

    test

--- app/config/config.php
+++ app/config/config.php
@@ -14,8 +14,8 @@ return new \Phalcon\Config(array(
         'controllersDir' => __DIR__ . '/../../app/controllers/',
         'modelsDir'      => __DIR__ . '/../../app/models/',
         'viewsDir'       => __DIR__ . '/../../app/views/',
-        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
-        'libraryDir'     => __DIR__ . '/../../app/library/',
+//        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+//        'libraryDir'     => __DIR__ . '/../../app/library/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
 //        'baseUri'        => '/st/',
     )
