be4f124cc629537e5e30effbb3cd1fc1c48adae3
moji <nntsugu@gmail.com>
2015-02-05 20:38:17 +0900

    test

--- app/controllers/SpController.php
+++ app/controllers/SpController.php
@@ -5,6 +5,8 @@ class SpController extends \Phalcon\Mvc\Controller
 
     public function indexAction()
     {
+
+var_dump('indddddß');exit;
         $kimono_result = json_decode(file_get_contents('https://www.kimonolabs.com/api/1zt5pqx6?apikey=woZPkSkRFS6bkosi4zQSAykupYsDwXYW'));
 
         $this->view->results = $kimono_result->results->collection1;
